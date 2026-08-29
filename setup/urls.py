0f448c24d35917293b773c668fa357fb06bf3d0e
Faquir Tembe <faquirtembedev@gmail.com>
2026-08-29 12:31:40 +0200

    ligamos as rotas da app contas ao mapa principal do site em setup/urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('contas.urls')),            #ligamos as rotas da app contas ao mapa principal do site em setup/urls.py
 ]
